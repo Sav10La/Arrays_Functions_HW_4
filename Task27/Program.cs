5205f8c2880be0b5da4e8b7f8ca38f4bd79598d4
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-07-05 23:54:24 +0500

    add Task27 DigitSum

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+void Zadacha27()
+{
+    Console.WriteLine("Введите число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Сумма цифр числа {number} равно {DigitSum(number)}");
+}
+
+int DigitSum(int number)
+{
+    int sum = 0, i;
+    while (number > 0)
+    {
+        i = number % 10;
+        sum = sum + i;
+        number = number / 10;
+    }
+    return sum;
+}
+Zadacha27();
